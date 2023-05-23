d99035b5134b9fbf433cb1bdf19854034c9de838
US\obondarenko <obondarenko@costar.com>
2023-05-22 17:46:30 -0700

    changes

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,46 @@
+const featuresServiceUrl = 'https://ocobondarenko.dev.loopnet.com/services/features/pull'; // features url
+const featuresServiceHeaderValue = '170cf7e5-667a-49a4-a630-227d6e85daa0';
+
+
 // Creating a DevTools panel
 chrome.devtools.panels.create("LoopNet", "../icons/cs_32.png", "panel/devtools.html", panel => {
     panel.onShown.addListener( (extPanelWindow) => {
-        RenderHeadersTab(extPanelWindow);
+       // RenderHeadersTab(extPanelWindow);
+          renderFeaturesList(extPanelWindow);
     });
 });
+
+async function fetchFeaturesAsync(extPanelWindow) {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-FMS-Token' : featuresServiceHeaderValue 
+      }
+    };
+    
+    try {
+      const response = await fetch(featuresServiceUrl, options);
+      const data = await response.json();
+      let featuresObj = JSON.parse(data.FeaturesJson);
+      return featuresObj.Apps[0].Features;
+    } catch (error) {
+        let errorBlock = extPanelWindow.document.querySelector('#error-block');
+        //errorBlock.innerHTML = JSON.stringify(error);
+    }
+}
+
+async function renderFeaturesList(extPanelWindow) {
+    let html = '';
+    let featuresList = await fetchFeaturesAsync(extPanelWindow);
+    for(let i=0;i<featuresList.length;i++) {
+        html += '<li>'+ featuresList[i].NameId+'</li>'
+    }
+    let headersCont = extPanelWindow.document.querySelector('#modRequestFrame').contentDocument.querySelector('#headersCont');
+    headersCont.innerHTML = html;
+}
+
+
 
 function RenderHeadersTab(extPanelWindow) {
     let p1HeadersObj;
@@ -25,23 +62,3 @@ function RenderHeaderFields(headerName, headerValue) {
     return '<input class="headerName" value="'+ headerName +'" /> : <input class="headerValue" value="'+ headerValue +'" /><br/>';
 }
 
-/*setInterval(async() => {
-    if (count > 3) {
-        chrome.storage.local.set({ key: Date.now() });
-    }
-
-    chrome.storage.local.get(["key"]).then((result) => {
-        storageData.innerHTML = result.key;
-    });
-
-    count = count + 1;
-    countStr.innerHTML = count;
-    try {
-        const apiResponse = await axios.get(`${api}`);
-        response.innerHTML = apiResponse.data.title;
-
-      } catch (error) {
-        response.innerHTML = "We have no data for the country you have requested.";
-      }
-
-}, 3000);*/
